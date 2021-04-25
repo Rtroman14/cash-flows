2467600bc8df3c8e9a86fa2fa8727014a32bc512
Ryan Roman <rtroman14@gmail.com>
2021-04-25 17:04:05 -0600

    financialContext change successful

--- web/src/context/FinancialContext.js
+++ web/src/context/FinancialContext.js
@@ -14,15 +14,20 @@ export function FinancialProvider(props) {
     });
 
     useEffect(() => {
-        // watch userData for changes
-        // make changes to userData, then update tableData
-
-        // always filter userData based on tableData.category and sortCost
         // if stall time when updating database before changes are reflected to interface then render loading component
 
-        // financialContext
-        console.log(`tableData changed!`);
-    }, [tableData]);
+        const displayTable =
+            tableData.category === "all"
+                ? userData
+                : userData.filter(row => row.category === tableData.category);
+
+        setTableData({
+            ...tableData,
+            table: displayTable,
+        });
+
+        console.log(`userData changed!`);
+    }, [userData, tableData.category]);
 
     const [emergencyFund, setEmergencyFund] = useState(
         userData.filter(row => row.category === "needs").reduce((a, b) => a + b.cost, 0) * 6
@@ -30,35 +35,21 @@ export function FinancialProvider(props) {
 
     const editCell = (id, field, newValue) => {
         let value = field === "cost" ? Number(newValue) : newValue;
+        const editedRow = userData.map(row => (row.id === id ? { ...row, [field]: value } : row));
 
-        const editedRow = tableData.table.map(row =>
-            row.id === id ? { ...row, [field]: value } : row
-        );
-        const editedRow2 = userData.map(row => (row.id === id ? { ...row, [field]: value } : row));
-
-        setUserData(editedRow2);
-        setTableData({
-            ...tableData,
-            table: editedRow,
-        });
+        setUserData(editedRow);
     };
 
     const deleteRow = id => {
         const removedRow = userData.filter(row => row.id !== id);
-        const removedRow2 = tableData.table.filter(row => row.id !== id);
 
         setUserData(removedRow);
-
-        setTableData({
-            ...tableData,
-            table: removedRow2,
-        });
     };
 
     const sortRows = () => {
         let sortCost;
 
-        const sortedRows = userData.sort((a, b) => {
+        const sortedRows = tableData.table.sort((a, b) => {
             if (tableData.sortCost === "DESC" || "") {
                 sortCost = "ASC";
 
@@ -77,23 +68,15 @@ export function FinancialProvider(props) {
     };
 
     const filterByCategory = category => {
-        const showCategory =
-            category === "all" ? userData : userData.filter(row => row.category === category);
-
         setTableData({
             ...tableData,
             category,
-            table: showCategory,
         });
     };
 
     const addRow = (expense, cost, category) => {
         const newRow = { id: uuidv4(), expense, cost: Number(cost), category };
         setUserData([...userData, newRow]);
-        setTableData({
-            ...tableData,
-            table: [...tableData.table, newRow],
-        });
     };
 
     const [income, setIncome] = useState({
