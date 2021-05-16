10256529c1eddbd3fcb8c363091902a9666fbd27
Ryan Roman <rtroman14@gmail.com>
2021-05-16 17:52:22 -0600

    finish userReducer logic

--- web/src/context/finance/FinanceContext.js
+++ web/src/context/finance/FinanceContext.js
@@ -1,10 +1,75 @@
-import React, { createContext } from "react";
+import React, { createContext, useReducer } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+import { userDataReducer } from "./reducers";
+import { ADD_ROW, DELETE_ROW, EDIT_CELL } from "./actions";
+
+import { data } from "../data";
 
 export const FinanceContext = createContext();
 
 export function FinanceProvider(props) {
+    const initialState = {
+        table: data,
+        sortCost: "",
+    };
+
+    const [state, dispatch] = useReducer(userDataReducer, initialState);
+
+    // add row
+    const addRow = row => {
+        const newRow = {
+            id: uuidv4(),
+            expense: row.expense,
+            cost: Number(row.cost),
+            category: row.category,
+        };
+        const newUserData = [...state.data, newRow];
+        const updatedWants = updateWants(newUserData);
+
+        dispatch({
+            type: ADD_ROW,
+            payload: updatedWants,
+        });
+    };
+
+    // delete row
+    const deleteRow = id => {
+        const removedRow = userData.data.filter(row => row.id !== id);
+        const updatedWants = updateWants(removedRow);
+
+        dispatch({
+            type: DELETE_ROW,
+            payload: updatedWants,
+        });
+    };
+
+    // edit cell
+    const editCell = (id, field, newValue) => {
+        const value = field === "cost" ? Number(newValue) : newValue;
+        const editedRow = userData.data.map(row =>
+            row.id === id ? { ...row, [field]: value } : row
+        );
+        const updatedWants = updateWants(editedRow);
+
+        dispatch({
+            type: EDIT_CELL,
+            payload: updatedWants,
+        });
+    };
+
+    const updateWants = newUserData => {
+        const wantsCost =
+            income.net -
+            newUserData.filter(row => row.id !== "leftoverWants").reduce((a, b) => a + b.cost, 0);
+
+        return newUserData.map(row =>
+            row.id === "leftoverWants" ? { ...row, cost: wantsCost } : row
+        );
+    };
+
     return (
-        <FinanceContext.Provider value={{ message: "Hello from context" }}>
+        <FinanceContext.Provider value={{ addRow, deleteRow, editCell }}>
             {props.children}
         </FinanceContext.Provider>
     );
--- web/src/context/finance/reducers.js
+++ web/src/context/finance/reducers.js
@@ -1,66 +1,14 @@
-import React, { createContext } from "react";
-import { v4 as uuidv4 } from "uuid";
-
-import { data } from "../data";
-
-export const FinancialContext = createContext();
-const initialState = {
-    table: data,
-    sortCost: "",
-};
-
-export const actions = {
-    ADD_ROW: "ADD_ROW",
-    DELETE_ROW: "DELETE_ROW",
-    EDIT_CELL: "EDIT_CELL",
-};
-
-const addRow = (row, state) => {
-    const newRow = {
-        id: uuidv4(),
-        expense: row.expense,
-        cost: Number(row.cost),
-        category: row.category,
-    };
-    const newUserData = [...state.data, newRow];
-    const updatedWants = updateWants(newUserData);
-
-    return { ...state, data: updatedWants };
-};
-
-const deleteRow = (id, state) => {
-    const removedRow = userData.data.filter(row => row.id !== id);
-    const updatedWants = updateWants(removedRow);
-
-    return { ...state, data: updatedWants };
-};
-
-const editCell = (id, field, newValue) => {
-    const value = field === "cost" ? Number(newValue) : newValue;
-    const editedRow = userData.data.map(row => (row.id === id ? { ...row, [field]: value } : row));
-    const updatedWants = updateWants(editedRow);
-
-    return { ...state, data: updatedWants };
-};
-
-const updateWants = newUserData => {
-    const wantsCost =
-        income.net -
-        newUserData.filter(row => row.id !== "leftoverWants").reduce((a, b) => a + b.cost, 0);
-
-    return newUserData.map(row => (row.id === "leftoverWants" ? { ...row, cost: wantsCost } : row));
-};
+import { ADD_ROW, DELETE_ROW, EDIT_CELL } from "./actions";
 
 export const userDataReducer = (state, action) => {
     switch (action.type) {
-        case actions.ADD_ROW:
-            return addRow(action.row, state);
-        case actions.DELETE_ROW:
-            return deleteRow(action.id, state);
-        case actions.EDIT_CELL:
-            return editCell(action.id, action.field, action.newValue, state);
+        case ADD_ROW:
+            return { ...state, data: action.payload };
+        case DELETE_ROW:
+            return { ...state, data: action.payload };
+        case EDIT_CELL:
+            return { ...state, data: action.payload };
         default:
             return state;
     }
 };
-// https://youtu.be/zxP4oGejqpU?t=1438
